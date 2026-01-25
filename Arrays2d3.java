4f59c79db11381bedc66d84b3f0cdaab3d7c4562
shrutikeshri2021 <shrutikeshri200423.uk@gmail.com>
2026-01-25 19:12:21 +0530

    Daily DSA: Add Arrays2d3.java

--- Arrays2d3.java
+++ Arrays2d3.java
@@ -60,3 +60,38 @@ public class Arrays2d3 {
             
     }
 }
+
+
+approach 2 optimized
+/*
+public class Arrays2d3 {
+
+    public static void diagonalMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += matrix[i][i];           // primary diagonal
+            sum += matrix[i][n - 1 - i];   // secondary diagonal
+        }
+
+        // subtract middle element once if matrix size is odd
+        if (n % 2 != 0) {
+            sum -= matrix[n / 2][n / 2];
+        }
+
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        diagonalMatrix(matrix);
+    }
+}
+*/
