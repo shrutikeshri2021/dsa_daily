4cd32d224de9525fa4dee66833527920db82bba6
shrutikeshri2021 <shrutikeshri200423.uk@gmail.com>
2026-02-03 12:15:08 +0530

    Update bitmani1.java

--- bitmani1.java
+++ bitmani1.java
@@ -9,9 +9,44 @@ public class bitmani1 {
         int bitmask=(1<<n);
         System.out.println(((number & bitmask)==0)?"0":"1");
     }
-    public static void setithbit(int n, int number){
+    public static int  setithbit(int n, int number){
         int bitmask=(1<<n);
+        int x= number | bitmask;
         System.out.println("set"+(number | bitmask));
+        return x;
+    }
+    public static int clearithbit(int n, int number){
+        int bitmask=~(1<<n);
+        int x=number & bitmask;
+        System.out.println("clear"+(number & bitmask));
+        return x;
+    }
+    public static void updateithbit(int n, int number, int newBit){
+        /*
+        if(newBit==0){
+            clearithbit(n,number);
+        }
+        else{
+            setithbit(n,number);
+        }
+ */
+        number=clearithbit(n,number);
+        int BitMask= newBit<<n;
+        System.out.println(number|BitMask);
+    }
+
+    public static void clearIBits(int n, int number){
+        int BitMask= ~(0) <<n;
+        System.out.println(number& BitMask);
+    }
+    public static void clearRangeBits(int i,int j, int number){
+        int n= ~(0) << (j+1);
+        System.out.println(Integer.toBinaryString(n));
+        int m = (1<<(i))-1;
+        System.out.println(Integer.toBinaryString(m));
+        int BitMask= n|m;
+        System.out.println( Integer.toBinaryString(BitMask));
+        System.out.println(Integer.toBinaryString(number & BitMask));
     }
 
     public static void main(String args[]){
@@ -20,6 +55,8 @@ public class bitmani1 {
         System.out.println(~5);System.out.println(5 & 6);System.out.println(5|6);
         System.out.println(5^6);System.out.println(5 <<2);System.out.println(5>>2);
        */
+        clearRangeBits(2,7, 0b100111010011);
+        /*
         evenodd(5);
         evenodd(0);
         evenodd(6);
@@ -39,6 +76,9 @@ public class bitmani1 {
         setithbit(1,10);
         setithbit(2,10);
         setithbit(3,10);
-
+        clearithbit(1,10);
+        updateithbit(2,10,1);
+        clearIBits(3,15);
+    */
     }
 }
